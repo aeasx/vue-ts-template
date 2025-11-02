de2d5b325aef1bd941cec2df81b638f1a2bf53ab
aeasx <3089326386@qq.com>
2025-11-02 16:11:05 +0800

    chore: 作用域插槽父子数据通信

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
+import Child from './components/Child.vue';
 </script>
 
 <template>
   <div>
     <h1>App.vue</h1>
     <Icon icon="mdi-light:home" />
+    <Child v-slot="slotProps">
+      来自子组件的数据
+      <span class="text-red-400">
+        {{ slotProps.childData }}
+      </span>
+    </Child>
   </div>
 </template>
